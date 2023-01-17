342b2a2941d115d246fe67fde88b43bafe646cd8
aneiderhiser <anneiderhiser@gmail.com>
2023-01-17 12:26:23 -0700

    updated LOS in encounter_augmented

--- models/readmissions__encounter_augmented.sql
+++ models/readmissions__encounter_augmented.sql
@@ -17,7 +17,7 @@ select
     aa.facility_npi,
     aa.ms_drg_code,
     aa.paid_amount,
-    {{ dbt.datediff("aa.discharge_date", "aa.admit_date","day") }} as length_of_stay,
+    {{ dbt.datediff("aa.admit_date", "aa.discharge_date","day") }} as length_of_stay,
     case
         when bb.encounter_id is not null then 1
 	    else 0
